137a54064050eb0e0982d1e02137a825ea387c02
Juliane <blier.juliane@gmail.com>
2020-01-12 20:55:00 +0100

    feat(exo-1): added first exercise about templating, data binding ans conditionnal templating
    #

---
+++ src/views/exo-1/Exercise1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="exercise-1">
+    <h1>Bienvenue, inconnu.e !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Exercise1',
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../../theme.styl';
+
+.exercise-1 h1 {
+  color: $grey;
+}
+</style>
